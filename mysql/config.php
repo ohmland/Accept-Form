8fd1973a74b554f2d0092d07d0736995218aadd4
natthakit <natthakitb1@gmail.com>
2020-05-14 01:44:27 +0700

    describe readme

--- mysql/config.php
+++ mysql/config.php
@@ -1,6 +1,11 @@
 <?php 
-  $dbhost="localhost";
-  $dbuser="root";
-  $dbpass="";
-  $dbname="test_dynamic_db";
+  // $dbhost=$_ENV["DB_HOST"];
+  // $dbname=$_ENV["DB_DATABASE"];
+  // $dbuser=$_ENV["DB_USERNAME"];
+  // $dbpass=$_ENV["DB_PASSWORD"];
+  
+  $dbhost = getenv('DB_HOST', true);
+  $dbname= getenv('DB_DATABASE');
+  $dbuser= getenv('DB_USERNAME');
+  $dbpass= getenv('DB_PASSWORD');
 ?>
